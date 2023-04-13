d58ead0f726829acf69cc29204f80c3372721c3f
JoaoPeDroPortoB <joaoPortoWork@hotmail.com>
2023-04-13 14:57:43 -0300

    Ultima alteração firebase infos

--- libs/fireBase.ts
+++ libs/fireBase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9p4hSMDe2fT7o-9Ar5_pufd0yNoUow0w",
-    authDomain: "gallery-69f20.firebaseapp.com",
-    projectId: "gallery-69f20",
-    storageBucket: "gallery-69f20.appspot.com",
-    messagingSenderId: "444317630581",
-    appId: "1:444317630581:web:b288b37f9e01fccca2b83c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
 
